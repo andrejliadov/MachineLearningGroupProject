d025c75f09d66d99a8e91b0d3fb29c014a37540c
Andrej <andrej.liadov@gmail.com>
2020-12-09 01:02:25 +0000

    Added simple text categorization and testing 	modified:   MachineLearningGroupProject/process_data.py 	new file:   MachineLearningGroupProject/simple.py

--- MachineLearningGroupProject/process_data.py
+++ MachineLearningGroupProject/process_data.py
@@ -5,7 +5,8 @@ import glob
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import nltk
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import stopwords
 
 #Parse words in the text files into their catagories
 def parse_data(catagory):
@@ -20,11 +21,18 @@ def parse_data(catagory):
 
     return text_data_list
 
+#Combine parsed data into one list per catagory
+def combine_lists(text_list):
+    text_data = []
+    for i in range(0, len(text_list)):
+        text_data = text_data + text_list
+
+    return text_data
+
 #Return a feature vector for each document
-def get_feature_vector(document_stem_list):
+def get_feature_vector(document_stem_list, vectorizer):
     tfidf_vector_list = []
 
-    vectorizer = TfidfVectorizer(norm=None)
     for i in range(0, len(document_stem_list)):
         tfidf_vector_list.append(vectorizer.fit_transform(document_stem_list[i]))
         #print(vectorizer.get_feature_names())
@@ -32,46 +40,82 @@ def get_feature_vector(document_stem_list):
     return tfidf_vector_list
 
 #Download a stop words list and tokenise the data
-def tokenize_data(document_list):
+def tokenize_data(text_data):
     nltk.download('stopwords')
-    vectorizer = CountVectorizer(stop_words=nltk.corpus.stopwords.words('english'))
+    vectorizer = CountVectorizer(stop_words=stopwords.words('english'))
+    tokenizer = vectorizer.build_tokenizer()
+    
+    tokens_list = tokenizer(text_data)
+
+    return tokens_list
+
+#Download a stop words list and tokenise a list of documents
+def tokenize_data_list(document_list):
+    nltk.download('stopwords')
+    vectorizer = CountVectorizer(stop_words=stopwords.words('english'))
     tokenizer = vectorizer.build_tokenizer()
     document_tokens_list = []
 
     for i in range(0, len(document_list)):
-        document_tokens_list.append(tokenizer(document_list[i]))
+        text = document_list[i]
+        text = text.lower()
+        tokens = tokenizer(text)
+        document_tokens_list.append(tokens)
 
     return document_tokens_list
 
+#Convert the tokens into stems
 def stem_data(document_tokens_list):
     stemmer = PorterStemmer()
     document_stems_list = []
 
     for i in range(0, len(document_tokens_list)):
         stem_list = []
-        for token in document_tokens_list[0]:
+        for token in document_tokens_list[i]:
             stem = stemmer.stem(token)
             stem_list.append(stem)
+        
         document_stems_list.append(stem_list)
     
     return document_stems_list
+
+#Convert the tokens into lemmas
+def lemmatise_data(document_tokens_list):
+    nltk.download('wordnet')
+    lemmatiser = WordNetLemmatizer()
+    document_lemmas_list = []
+
+    for i in range(0, len(document_tokens_list)):
+        lemma_list = []
+        for token in document_tokens_list[i]:
+            lemma = lemmatiser.lemmatize(token)
+            lemma_list.append(lemma)
+        document_lemmas_list.append(lemma_list)
+
+    return document_lemmas_list
 
 #The data is parsed here
 culture_text_list = parse_data('Culture')
 geography_text_list = parse_data('Geography')
 physics_text_list = parse_data('Physics')
 
+#Combine words on a topic
+#culture_text_data = combine_lists(culture_text_list)
+#geography_text_data = combine_lists(geography_text_list)
+#physics_text_data = combine_lists(physics_text_list)
+
 #Tokenise the data
-culture_tokens_list = tokenize_data(culture_text_list)
-geography_tokens_list = tokenize_data(geography_text_list)
-physics_tokens_list = tokenize_data(physics_text_list)
+culture_tokens_list = tokenize_data_list(culture_text_list)
+geography_tokens_list = tokenize_data_list(geography_text_list)
+physics_tokens_list = tokenize_data_list(physics_text_list)
 
 #Apply a Porter Stemming to the tokens
-culture_stems_list = stem_data(culture_tokens_list)
-geography_stems_list = stem_data(geography_tokens_list)
-physics_stems_list = stem_data(physics_tokens_list)
+culture_stems_list = lemmatise_data(culture_tokens_list)
+geography_stems_list = lemmatise_data(geography_tokens_list)
+physics_stems_list = lemmatise_data(physics_tokens_list)
 
 #Get the feature vectors for documents in every catagory
-culture_features = get_feature_vector(culture_stems_list)
-culture_features = get_feature_vector(geography_stems_list)
-culture_features = get_feature_vector(physics_stems_list)
+vectorizer = TfidfVectorizer(norm=None, max_features=5000)
+culture_features = get_feature_vector(culture_stems_list, vectorizer)
+geography_features = get_feature_vector(geography_stems_list, vectorizer)
+physics_features = get_feature_vector(physics_stems_list, vectorizer)
---
+++ MachineLearningGroupProject/simple.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.svm import LinearSVC
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+
+#Read in the data
+DATA_DIR = "./data/"
+data = load_files(DATA_DIR, encoding='utf-8', decode_error='replace')
+labels, counts = np.unique(data.target, return_counts=True)
+labels_str = np.array(data.target_names)[labels]
+print(dict(zip(labels_str, counts)))
+
+#Tokenise and lemmatise the text data
+nltk.download('wordnet')
+lemmatiser = WordNetLemmatizer()
+tokeniser = CountVectorizer().build_tokenizer()
+for i in range(0, len(data.data)):
+    temp_str = " "
+    data.data[i] = data.data[i].lower()
+    data.data[i] = tokeniser(data.data[i])
+    for token in range(0, len(data.data[i])):
+        data.data[i][token] = lemmatiser.lemmatize(data.data[i][token])
+    data.data[i] = temp_str.join(data.data[i])
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(data.data, data.target)
+
+vectoriser = TfidfVectorizer(stop_words='english', max_features=10000, decode_error='ignore', sublinear_tf=True, ngram_range=(1,2))
+vectoriser.fit(X_train)
+
+model = LinearSVC()
+model.fit(vectoriser.transform(X_train), Y_train)
+
+y_pred = model.predict(vectoriser.transform(X_test))
+print(accuracy_score(Y_test, y_pred))
+print(classification_report(Y_test, y_pred))
