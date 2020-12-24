07ea5133f4fcf1387bc32ffc8e958bce2898d53c
lenilorac <carlzzzlk@gmail.com>
2020-12-24 15:01:26 +0000

    Update one_hot.py

--- one_hot.py
+++ one_hot.py
@@ -1,30 +1,39 @@
 import numpy as np
+# import matplotlib.pyplot as plt
 from sklearn.datasets import load_files
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
 #Read in the data
-DATA_DIR = "MachineLearningGroupProject/data/"
+DATA_DIR = "./data/"
 data = load_files(DATA_DIR, encoding='utf-8', decode_error='replace')
 labels, counts = np.unique(data.target, return_counts=True)
 labels_str = np.array(data.target_names)[labels]
 print(dict(zip(labels_str, counts)))
 
-label_encoder = LabelEncoder()
+#Tokenise and lemmatise the text data
+le = LabelEncoder()
+onehot_encoder = OneHotEncoder(sparse=False, handle_unknown = 'ignore')
+lemmatiser = WordNetLemmatizer()
 tokeniser = CountVectorizer().build_tokenizer()
-integer_encoder = label_encoder
-enc = OneHotEncoder(sparse=False, handle_unknown = 'ignore')
-array = []
 for i in range(0, len(data.data)):
+    temp_str = " "
     data.data[i] = data.data[i].lower()
     data.data[i] = tokeniser(data.data[i])
-    integer_encoded = integer_encoder.fit_transform(data.data[i])
-    array.append(integer_encoded.reshape(len(integer_encoded), 1))
+    for token in range(0, len(data.data[i])):
+        data.data[i][token] = lemmatiser.lemmatize(data.data[i][token])
+    data.data[i] = temp_str.join(data.data[i])
 
-onehot_encoded = []
+label_encoder = le.fit_transform(data.data)
+integer_encoder = label_encoder.reshape(len(label_encoder), 1)
+onehot_encoder = onehot_encoder.fit_transform(integer_encoder)
 
-for i in range(0, len(array)):
-    onehot_encoded.append(enc.fit_transform(array[i]))
-
+X_train, X_test, Y_train, Y_test = train_test_split(data.data, data.target)
